5cae08731a1364d1e67d8494a97c286a38130b4c
Austin Moran <moraustin@gmail.com>
2013-03-11 21:53:29 -0400

    added room.h

---
+++ room.h
@@ -0,0 +1,27 @@
+#ifndef GUARD__ROOM_H
+#define GUARD__ROOM_H
+
+#include <string>
+#include "color.h"
+
+
+namespace hcal{
+
+    class Room{
+    public:
+        int id;
+        std::string name;
+        Color color;
+        
+        Room(){
+           //defalt member constructors
+        }
+
+        Room(int param_id, std::string param_name, std::string param_hex_color){
+            id = param_id;
+            name = param_name;
+            color = Color(param_hex_color);
+        }
+    }; 
+}
+#endif
